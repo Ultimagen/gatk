5c0ab1b380205e3f1c91ed581170f623e07a2676
Ilya Soifer <ilya.soifer@ultimagenome.com>
2019-07-17 13:32:49 +0300

    Updated .gitignore
    Fixed a bug in hardClipCigar function that caused incorrect cigar calculation
    in case of deletions in the read
    
    Added flow based aligner and read support
    
    Fixed a bug in hard clipping cigar
    
    Added functions for flow based reads
    
    Fixed FlowBasedRead when the number of flows is larger than 256
    
    Working version regressed + motif
    
    Fixed a bug in clipping hmer
    
    Fixed cases when the reads had hanging ends over the haplotypes
    
    Fixed boundary case when trimmed read is very short
    
    Normalized to maximum
    
    Fit the version of GATK to Matt's recalibration format
    
    Added unit tests for clipping and fixed more hardclipping bugs
    
    Added test for clippoing
    
    Fixed a bug in hard clipping cigar
    
    Fixed a bug in hardClipCigar function that caused incorrect cigar calculation
    in case of deletions in the read
    
    Added unit tests for clipping and fixed more hardclipping bugs
    
    Added test for clippoing
    
    Added test for hard clipping if there are Ns inside the CIGAR
    
    1. Changed ReadClipper unit tests:
       - The tests in many cases assumed that the unclipped alignment locations do not    change when the read is clipped. This is not true: for example if start for cigar
       1M1I3M is 100, the unclipped start for 2H3M is 99. All assertUnclipped calls were
       removed
       - Alignment now check that the read length remains to be consistent with the CIGAR, that the aligned bases span are consistent with the CIGAR and that the number of clipped bases from the read is consistent with the requested clipping
    2. Hard clipping in ClippingOps was buggy, thus we introduced new tests for it.
    3. Text was refactored for readability
    4. Clipping in ClippingOps did not treat insertions and deletions in the clipped parts of the CIGAR correctly. This was fixed
    5. Alignment re-calculation after clipping did not work correctly if the initial CIGAR contained insertions and deletions
    5. Hard clipping applied to the hard clipped read did not behave correctly
    
    Fixed comments
    
    Patch for haplotypes that contain Ns
    
    PR comments
    
    Fixed a bug in ReadClipper that occured when match was followed by deletion and less than the length of match was clipped
    
    Added non-zero values for empty cells in flow matrix
    
    Temporary commit
    
    Updated max class to 12
    
    Added looser clipping restriction
    
    Probabilistic version of SOR
    
    Moved files to make them disoverable
    
    Small fixes for allele specific SOR
    
    Fixed annotation name
    
    Cleaning up magic numbers
    
    Fixed broken dependencies
    
    Fixed case when max_class is not defined in the header
    
    Fixed a boundary case
    
    Decreased informative reads threshold
    
    Fixed another boundary condition
    
    Decreased informative threshold again
    
    Fixed cases when for some reason flow order is not given
    
    Changed informative read threshold again

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCaller.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCaller.java
@@ -147,7 +147,9 @@ public final class HaplotypeCaller extends AssemblyRegionWalker {
     /**
      * A raw, unfiltered, highly sensitive callset in VCF format.
      */
-    @Argument(fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME, shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME, doc = "File to which variants should be written")
+    @Argument(fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME,
+            shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
+            doc = "File to which variants should be written")
     public GATKPath outputVCF = null;
 
     private VariantContextWriter vcfWriter;
---
+++ src/main/java/org/ultimagenomics/flow_based_read/tests/test_haplotype_matching.java
@@ -0,0 +1,74 @@
+package org.ultimagenomics.flow_based_read.tests;
+
+import htsjdk.samtools.SAMFileHeader;
+import org.broadinstitute.hellbender.utils.genotyper.AlleleLikelihoods;
+import org.ultimagenomics.flow_based_read.alignment.FlowBasedAlignmentEngine;
+import org.ultimagenomics.flow_based_read.read.FlowBasedHaplotype;
+import org.ultimagenomics.flow_based_read.read.FlowBasedRead;
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SamReader;
+import htsjdk.samtools.SamReaderFactory;
+import org.broadinstitute.hellbender.utils.haplotype.Haplotype;
+import org.broadinstitute.hellbender.utils.read.GATKRead;
+import org.broadinstitute.hellbender.utils.read.SAMRecordToGATKReadAdapter;
+import org.genomicsdb.importer.model.ChromosomeInterval;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class test_haplotype_matching {
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        System.out.println("Input reads file: " + args[0]);
+        System.out.println("Input haplotypes file: " + args[1]);
+        System.out.println("CR: " + args[2]);
+        System.out.println("Output prefix: " + args[1]);
+        final SamReader reader = SamReaderFactory.makeDefault().open(new File(args[0]));
+        ArrayList<GATKRead> reads = new ArrayList<>();
+        ArrayList<Haplotype> haplotypes = new ArrayList<>();
+        int count_reads = 0;
+        int count_haplotypes = 0;
+        ArrayList<SAMFileHeader> headers = new ArrayList<>();
+        for ( final SAMRecord rec : reader ) {
+            SAMRecordToGATKReadAdapter tmp = new SAMRecordToGATKReadAdapter(rec);
+            if (!tmp.getAttributeAsString("RG").startsWith("ArtificialHaplotype")) {
+                if (tmp.getAttributeAsString(("CR")).equals(new String(args[2]))) {
+                    reads.add(tmp);
+                    count_reads++;
+                }
+            } else {
+                if (tmp.getAttributeAsString(("CR")).equals(new String(args[2]))) {
+                    ChromosomeInterval gl = new ChromosomeInterval(tmp.getContig(), tmp.getStart(), tmp.getEnd());
+
+                    Haplotype hap = new Haplotype(tmp.getBases(), gl);
+                    hap.setCigar(tmp.getCigar());
+                    haplotypes.add(hap);
+                    count_haplotypes++;
+                }
+            }
+
+        }
+
+        System.out.println(String.format("%d reads %d haplotypes", count_reads, count_haplotypes));
+        ArrayList<FlowBasedRead> fbrs = new ArrayList<>();
+        int cnt = 0 ;
+        for (final GATKRead r :  reads) {
+            fbrs.add(new FlowBasedRead(r, "TACG", 8));
+        }
+        for (FlowBasedRead fbr : fbrs ) fbr.apply_alignment();
+
+        ArrayList<FlowBasedHaplotype> fbhs = new ArrayList<>();
+        for (final Haplotype hap : haplotypes) {
+            fbhs.add(new FlowBasedHaplotype(hap, "TACG", 8));
+        }
+
+        FlowBasedAlignmentEngine fbe = new FlowBasedAlignmentEngine(-5);
+
+        final AlleleLikelihoods<GATKRead, Haplotype> haplotypeReadLikelihoods =
+                fbe.computeReadLikelihoods(haplotypes, reads);
+
+    }
+
+}
