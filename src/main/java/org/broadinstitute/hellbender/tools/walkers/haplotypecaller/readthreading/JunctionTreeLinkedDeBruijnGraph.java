397a7c52878e67b6ce3a926abb632354a94181c7
Yossi Farjoun <farjoun@broadinstitute.org>
2019-12-10 13:18:31 +0200

    Add read filter that filters read with a tag that satisfied a numerical condition

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/readthreading/JunctionTreeLinkedDeBruijnGraph.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/readthreading/JunctionTreeLinkedDeBruijnGraph.java
@@ -569,7 +569,7 @@ public class JunctionTreeLinkedDeBruijnGraph extends AbstractReadThreadingGraph 
         }
 
         /**
-         * Returns the root node for this edge (ensuring that it has had its count intcremented to keep track of the total evidence spanning this tree.
+         * Returns the root node for this edge (ensuring that it has had its count incremented to keep track of the total evidence spanning this tree.
          *
          * @return
          */
