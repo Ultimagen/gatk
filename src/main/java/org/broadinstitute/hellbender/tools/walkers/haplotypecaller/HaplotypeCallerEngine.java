fa286fdeb49c472fb50e00ac3bb058595868a999
Dror Kessler <dror27.kessler@gmail.com>
2020-03-08 09:18:23 +0200

    debugging aids

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCallerEngine.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCallerEngine.java
@@ -64,6 +64,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -790,7 +791,7 @@ public final class HaplotypeCallerEngine implements AssemblyRegionEvaluator {
         }
 
         final CalledHaplotypes calledHaplotypes = genotypingEngine.assignGenotypeLikelihoods(
-                refView == null ? haplotypes : refView.uncollapseByRef(haplotypes),
+                haplotypes,
                 readLikelihoods,
                 perSampleFilteredReadList,
                 assemblyResult.getFullReferenceWithPadding(),
@@ -802,6 +803,18 @@ public final class HaplotypeCallerEngine implements AssemblyRegionEvaluator {
                 hcArgs.maxMnpDistance,
                 readsHeader,
                 haplotypeBAMWriter.isPresent());
+
+        // uncollapse?
+        if ( calledHaplotypes.getCalledHaplotypes().size() != 0 && refView != null )
+        {
+            AtomicInteger       changeCount = new AtomicInteger();
+            List<Haplotype>     uncollapsedHaplotypes = refView.uncollapseByRef(calledHaplotypes.getCalledHaplotypes(), changeCount);
+
+            if ( changeCount.get() != 0 ) {
+                calledHaplotypes.getCalledHaplotypes().clear();
+                calledHaplotypes.getCalledHaplotypes().addAll(uncollapsedHaplotypes);
+            }
+        }
 
         if ( haplotypeBAMWriter.isPresent() ) {
             final Set<Haplotype> calledHaplotypeSet = new HashSet<>(calledHaplotypes.getCalledHaplotypes());
