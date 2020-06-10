84a52c3cd235be87eaa2b2dac155f0ac5400b879
Ilya Soifer <ilya.soifer@ultimagenome.com>
2020-06-10 17:54:17 +0300

    Fixed a boundary case of hmer called higher than the maxHmer

--- src/main/java/org/ultimagenomics/flow_based_read/read/FlowBasedRead.java
+++ src/main/java/org/ultimagenomics/flow_based_read/read/FlowBasedRead.java
@@ -265,8 +265,9 @@ public class FlowBasedRead extends SAMRecordToGATKReadAdapter implements GATKRea
         for ( int i = 0 ; i < kh.length; i++ ) {
             if (( kh[i] & 0xff )> maxHmer) {
                 flowMatrix[maxHmer][kf[i]]=1;
+            } else {
+                flowMatrix[kh[i] & 0xff][kf[i]] = kd_probs[i];
             }
-            flowMatrix[kh[i] & 0xff][kf[i]] = kd_probs[i];
         }
 
     }
