0742f0e1fb94a9e91a5a39b29cde8a55bf318e6d
Ilya Soifer <ilya.soifer@ultimagenome.com>
2020-07-26 21:48:14 +0300

    Fixed google access (WIP)

--- src/main/java/org/ultimagenomics/variant_calling/HaplotypeBasedVariantRecallerArgumentCollection.java
+++ src/main/java/org/ultimagenomics/variant_calling/HaplotypeBasedVariantRecallerArgumentCollection.java
@@ -1,24 +1,9 @@
 package org.ultimagenomics.variant_calling;
 
-import htsjdk.variant.variantcontext.VariantContext;
-import org.broadinstitute.barclay.argparser.Advanced;
 import org.broadinstitute.barclay.argparser.Argument;
-import org.broadinstitute.barclay.argparser.ArgumentCollection;
-import org.broadinstitute.barclay.argparser.Hidden;
-import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
-import org.broadinstitute.hellbender.cmdline.argumentcollections.DbsnpArgumentCollection;
-import org.broadinstitute.hellbender.engine.FeatureInput;
-import org.broadinstitute.hellbender.tools.walkers.genotyper.StandardCallerArgumentCollection;
-import org.broadinstitute.hellbender.tools.walkers.haplotypecaller.AssemblyBasedCallerArgumentCollection;
-import org.broadinstitute.hellbender.tools.walkers.haplotypecaller.HaplotypeCallerEngine;
-import org.broadinstitute.hellbender.tools.walkers.haplotypecaller.HaplotypeCallerReadThreadingAssemblerArgumentCollection;
-import org.broadinstitute.hellbender.tools.walkers.haplotypecaller.ReadThreadingAssemblerArgumentCollection;
-import org.ultimagenomics.flow_based_read.utils.FlowBasedAlignmentArgumentCollection;
 
 import java.io.File;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Set of arguments for the {@link HaplotypeBasedVariantRecaller}
@@ -36,13 +21,13 @@ public class HaplotypeBasedVariantRecallerArgumentCollection implements Serializ
      *  This argument specifies a BAM file with Reads to recall from
      **/
     @Argument(fullName = "reads-file-bam", doc = "BAM file containing reads", optional = false)
-    public File READS_BAM_FILE = null;
+    public String READS_BAM_FILE = null;
 
     /**
      *  This argument specifies a BAM file with Haplotypes to limit reads on
      **/
     @Argument(fullName = "haplotypes-file-bam", doc = "BAM file containing haplotypes", optional = false)
-    public File HAPLOTYPES_BAM_FILE = null;
+    public String HAPLOTYPES_BAM_FILE = null;
 
     /**
      *  This argument specifies a CSV to be filled with likelihood matrix data
