de4ffcf33845f3e59dc67f9aa2154dc3946046dd
Dror Kessler <dror27.kessler@gmail.com>
2020-10-28 11:36:39 +0000

    simple getOffsetOnRead test

---
+++ src/test/java/org/ultimagenomics/variant_calling/VariantRecallerResultWriterTest.java
@@ -0,0 +1,44 @@
+package org.ultimagenomics.variant_calling;
+
+import htsjdk.samtools.SAMFileHeader;
+import htsjdk.samtools.SAMReadGroupRecord;
+import htsjdk.samtools.util.Locatable;
+import org.broadinstitute.hellbender.GATKBaseTest;
+import org.broadinstitute.hellbender.engine.AlignmentContext;
+import org.broadinstitute.hellbender.exceptions.UserException;
+import org.broadinstitute.hellbender.utils.SimpleInterval;
+import org.broadinstitute.hellbender.utils.pileup.ReadPileup;
+import org.broadinstitute.hellbender.utils.read.ArtificialReadUtils;
+import org.broadinstitute.hellbender.utils.read.GATKRead;
+import org.broadinstitute.hellbender.utils.read.ReadUtils;
+import org.broadinstitute.hellbender.utils.read.SAMRecordToGATKReadAdapter;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+public class VariantRecallerResultWriterTest extends GATKBaseTest {
+
+    @Test
+    public void testTest() throws Exception {
+
+        // create sam header
+        final SAMFileHeader header = ArtificialReadUtils.createArtificialSamHeader(1, 1, 1000);
+
+        // create reads
+        final GATKRead read2 = ArtificialReadUtils.createArtificialRead("10M1D10M1I10M");
+        int[]          ofsPairs = { 0, 0, 9, 9,
+                                    10, 9,
+                                    11, 10, 20, 19,
+                                    21, 20,
+                                    22, 22, 30, 30,
+                                    31, -1
+                                    };
+        for ( int n = 0 ; n < ofsPairs.length ; n += 2 )
+            Assert.assertEquals(VariantRecallerResultWriter.getOffsetOnRead(read2, ofsPairs[n]), ofsPairs[n+1]);
+
+
+    }
+}
