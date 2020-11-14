74e5a9a27e9c1da72bfce43292c54a6deb29e530
Dror Kessler <dror27.kessler@gmail.com>
2020-11-14 18:36:42 +0000

    MarkDuplicatesSpark logic aligned with Picard

--- src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/MarkDuplicatesSparkArgumentCollection.java
+++ src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/MarkDuplicatesSparkArgumentCollection.java
@@ -26,28 +26,33 @@ public final class MarkDuplicatesSparkArgumentCollection implements Serializable
     @Argument(fullName = MarkDuplicatesSparkArgumentCollection.DO_NOT_MARK_UNMAPPED_MATES_LONG_NAME, doc = "Enabling this option will mean unmapped mates of duplicate marked reads will not be marked as duplicates.")
     public boolean dontMarkUnmappedMates = false;
 
-    @Argument(fullName = MarkDuplicatesSparkArgumentCollection.DUPLICATE_TAGGING_POLICY_LONG_NAME, doc = "Determines how duplicate types are recorded in the DT optional attribute.", optional = true,
-              mutex = {REMOVE_ALL_DUPLICATE_READS, REMOVE_SEQUENCING_DUPLICATE_READS})
+    // added params
+    @Argument(doc = "tagging policy settings. values: DontTag, OpticalOnly, All. default: DontTag")
     public MarkDuplicates.DuplicateTaggingPolicy taggingPolicy = MarkDuplicates.DuplicateTaggingPolicy.DontTag;
 
-    @Argument(fullName = MarkDuplicatesSparkArgumentCollection.REMOVE_ALL_DUPLICATE_READS, doc = "If true do not write duplicates to the output file instead of writing them with appropriate flags set.",
-            mutex = {MarkDuplicatesSparkArgumentCollection.DUPLICATE_TAGGING_POLICY_LONG_NAME, MarkDuplicatesSparkArgumentCollection.REMOVE_SEQUENCING_DUPLICATE_READS}, optional = true)
+    @Argument(doc = "removeSequencingDuplicates. defualt: false")
+    public boolean removeSequencingDuplicates = false;
+
+    @Argument(doc = "removeAllDuplicates. default: false")
     public boolean removeAllDuplicates = false;
 
-    @Argument(fullName = MarkDuplicatesSparkArgumentCollection.REMOVE_SEQUENCING_DUPLICATE_READS, doc = "If true do not write optical/sequencing duplicates to the output file instead of writing them with appropriate flags set.",
-            mutex = {MarkDuplicatesSparkArgumentCollection.DUPLICATE_TAGGING_POLICY_LONG_NAME, MarkDuplicatesSparkArgumentCollection.REMOVE_ALL_DUPLICATE_READS}, optional = true)
-    public boolean removeSequencingDuplicates = false;
+    @Argument(doc = "Use specific quality summing strategy for flow based reads. The strategy ensures that the same " +
+            "(and correct) quality value is used for all bases of the same homopolymer. Default false.")
+    public boolean FLOW_QUALITY_SUM_STRATEGY = false;
 
     @Argument(doc = "Make end location of read be significant when considering duplicates, " +
             "in addition to the start location, which is always significant. Default false.")
     public boolean FLOW_END_LOCATION_SIGNIFICANT = false;
 
+    @Argument(doc = "Maximal number of bases of reads ends difference that is marked as match. Default 0.")
+    public int ENDS_READ_UNCERTAINTY = 0;
+
     @Argument(doc = "Use clipped, rather than unclipped, when considering duplicates. Default false.")
     public boolean FLOW_USE_CLIPPED_LOCATIONS = false;
 
-    @Argument(doc = "Skip first and last homopolymers, when considering duplicates. " +
-            " This argument takes precedence over FLOW_USE_CLIPPED_LOCATIONS. Default false.")
-    public boolean FLOW_SKIP_ENDS_HOMOPOLYMERS = false;
+    @Argument(doc = "Skip first N flows, when considering duplicates. Default 0.")
+    public int FLOW_SKIP_START_HOMOPOLYMERS = 0;
 
-
+    @Argument(doc = "Emit additional debugging info specific to ultima flow. Default fase.")
+    public boolean DEBUG_ULTIMA_DUPS = false;
 }
--- src/main/java/org/broadinstitute/hellbender/utils/read/markduplicates/sparkrecords/Fragment.java
+++ src/main/java/org/broadinstitute/hellbender/utils/read/markduplicates/sparkrecords/Fragment.java
@@ -1,6 +1,8 @@
 package org.broadinstitute.hellbender.utils.read.markduplicates.sparkrecords;
 
 import htsjdk.samtools.SAMFileHeader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.broadinstitute.hellbender.cmdline.argumentcollections.MarkDuplicatesSparkArgumentCollection;
 import org.broadinstitute.hellbender.tools.spark.transforms.markduplicates.MarkDuplicatesSpark;
 import org.broadinstitute.hellbender.tools.spark.transforms.markduplicates.MarkDuplicatesSparkUtils;
@@ -12,6 +14,7 @@ import org.broadinstitute.hellbender.utils.read.markduplicates.ReadsKey;
 import picard.sam.markduplicates.util.ReadEnds;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Class representing a single read fragment at a particular start location without a mapped mate.
@@ -29,10 +32,24 @@ public class Fragment extends TransientFieldPhysicalLocation {
     public Fragment(final GATKRead first, final SAMFileHeader header, int partitionIndex, MarkDuplicatesScoringStrategy scoringStrategy, Map<String, Byte> headerLibraryMap, final MarkDuplicatesSparkArgumentCollection mdArgs) {
         super(partitionIndex, first.getName());
 
+        int        start = first.isReverseStrand() ? ReadUtils.getSelectedRecordEnd(first, null, header, mdArgs) : ReadUtils.getSelectedRecordStart(first, null, header, mdArgs);
+        int        end = 0;
+        int        endUncert = 0;
+        if ( mdArgs.FLOW_END_LOCATION_SIGNIFICANT ) {
+            AtomicInteger endUncertainty = new AtomicInteger(mdArgs.ENDS_READ_UNCERTAINTY);
+            end = !first.isReverseStrand() ? ReadUtils.getSelectedRecordEnd(first, endUncertainty, header, mdArgs) : ReadUtils.getSelectedRecordStart(first, endUncertainty, header, mdArgs);
+            endUncert = endUncertainty.intValue();
+        }
+        if ( mdArgs.DEBUG_ULTIMA_DUPS )
+            System.out.println(String.format("[%s %b] : %d %d : %d %d : %d %d %d",
+                    first.getName(), first.isReverseStrand(),
+                    first.getUnclippedStart(), first.getUnclippedEnd(),
+                    first.getStart(), first.getEnd(),
+                    start, end, endUncert));
+
         this.score = scoringStrategy.score(first);
         this.R1R = first.isReverseStrand();
-        this.key = ReadsKey.getKeyForFragment(ReadUtils.getSelectedStart(first, mdArgs.FLOW_SKIP_ENDS_HOMOPOLYMERS, mdArgs.FLOW_USE_CLIPPED_LOCATIONS),
-                mdArgs.FLOW_END_LOCATION_SIGNIFICANT ? ReadUtils.getSelectedEnd(first, mdArgs.FLOW_SKIP_ENDS_HOMOPOLYMERS, mdArgs.FLOW_USE_CLIPPED_LOCATIONS) : 0,
+        this.key = ReadsKey.getKeyForFragment(start, end, endUncert,
                 isRead1ReverseStrand(),
                 (short)ReadUtils.getReferenceIndex(first, header),
                 headerLibraryMap.get(MarkDuplicatesSparkUtils.getLibraryForRead(first, header, LibraryIdGenerator.UNKNOWN_LIBRARY)));
