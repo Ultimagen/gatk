a74c7566b00112ea2e6637861572a1c0b5f74911
Ilya Soifer <ilya.soifer@ultimagen.com>
2021-05-12 11:02:58 +0300

    [BIOIN-115] Usage of multiple samples and normal samples in allele filtering (#48)
    * Using actual quality calculation rather than PLs
    
    * WIP
    
    * WIP - adding normal samples to Mutect
    
    * Added normal samples to mutect AF
    
    * Fixed an allele swap in mutect AF
    
    * Fixed correction directions, seems that the code now more or less works
    
    * Changed the logic in mutect allele filtering so that the normal likelihood is not treated differently from the tumor likelihood

--- src/main/java/org/ultimagen/haplotypeCalling/AlleleFilteringMutect.java
+++ src/main/java/org/ultimagen/haplotypeCalling/AlleleFilteringMutect.java
@@ -1,29 +1,51 @@
 package org.ultimagen.haplotypeCalling;
 
 import htsjdk.variant.variantcontext.Allele;
-import org.broadinstitute.hellbender.tools.walkers.mutect.M2ArgumentCollection;
-import org.broadinstitute.hellbender.tools.walkers.mutect.SomaticGenotypingEngine;
-import org.broadinstitute.hellbender.tools.walkers.mutect.SomaticLikelihoodsEngine;
-import org.broadinstitute.hellbender.tools.walkers.mutect.SubsettedLikelihoodMatrix;
+import org.broadinstitute.hellbender.tools.walkers.mutect.*;
 import org.broadinstitute.hellbender.utils.genotyper.AlleleLikelihoods;
+import org.broadinstitute.hellbender.utils.genotyper.AlleleList;
 import org.broadinstitute.hellbender.utils.genotyper.LikelihoodMatrix;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
 
 import java.io.OutputStreamWriter;
-import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 public class AlleleFilteringMutect extends AlleleFiltering {
     private SomaticGenotypingEngine genotypingEngine;
-    public AlleleFilteringMutect(M2ArgumentCollection _m2args, OutputStreamWriter assemblyDebugStream, SomaticGenotypingEngine _genotypingEngine){
+    public AlleleFilteringMutect(M2ArgumentCollection _m2args,
+                                 OutputStreamWriter assemblyDebugStream,
+                                 SomaticGenotypingEngine _genotypingEngine){
         super(_m2args, assemblyDebugStream);
         genotypingEngine = _genotypingEngine;
     }
 
-    int getAlleleLikelihood(final AlleleLikelihoods<GATKRead, Allele> alleleLikelihoods, Allele allele) {
-        final LikelihoodMatrix<GATKRead, Allele> initialMatrix = alleleLikelihoods.sampleMatrix(0);
-        final LikelihoodMatrix<GATKRead, Allele> logMatrixWithoutThisAllele = SubsettedLikelihoodMatrix.excludingAllele(alleleLikelihoods.sampleMatrix(0), allele);
 
+    int getAlleleLikelihood(final AlleleLikelihoods<GATKRead, Allele> alleleLikelihoods, Allele allele) {
+
+        final List<LikelihoodMatrix<GATKRead, Allele>> allMatrices = IntStream.range(0, alleleLikelihoods.numberOfSamples())
+                .mapToObj(alleleLikelihoods::sampleMatrix)
+                .collect(Collectors.toList());
+        final AlleleList<Allele> alleleList = allMatrices.get(0);
+        final LikelihoodMatrix<GATKRead, Allele> logAllMatrix = SomaticGenotypingEngine.combinedLikelihoodMatrix(allMatrices, alleleList);
+        double alleleLogOdds = somaticAltLogOdds(logAllMatrix);
+        logger.debug(() -> String.format("GAL:: %s: %f", allele.toString(), alleleLogOdds));
+        return (int)(10*alleleLogOdds);
+    }
+
+
+    /**
+     * Calculate the log likelihoods of the ref/alt het genotype for each alt allele, then subtracts
+     * these from the hom ref log likelihood to get the log-odds.
+     *
+     * @param matrix a matrix of log likelihoods
+     */
+    private double somaticAltLogOdds(final LikelihoodMatrix<GATKRead, Allele> matrix) {
+
+        final LikelihoodMatrix<GATKRead, Allele> initialMatrix = matrix;
+        final LikelihoodMatrix<GATKRead, Allele> logMatrixWithoutThisAllele = SubsettedLikelihoodMatrix.excludingAllele(matrix, matrix.getAllele(0));
 
         final double logEvidenceWithoutThisAllele = logMatrixWithoutThisAllele.evidenceCount() == 0 ? 0 :
                 SomaticLikelihoodsEngine.logEvidence(SomaticGenotypingEngine.getAsRealMatrix(logMatrixWithoutThisAllele),
@@ -31,13 +53,7 @@ public class AlleleFilteringMutect extends AlleleFiltering {
         final double logEvidenceWithAllAlleles= initialMatrix.evidenceCount() == 0 ? 0 :
                 SomaticLikelihoodsEngine.logEvidence(SomaticGenotypingEngine.getAsRealMatrix(initialMatrix),
                         genotypingEngine.makePriorPseudocounts(initialMatrix.numberOfAlleles()));
-
-        logger.debug(() -> String.format("GAL:: %s: %f %f %d", allele.toString(), logEvidenceWithAllAlleles,
-                logEvidenceWithoutThisAllele,
-                (int)(10*(-logEvidenceWithAllAlleles + logEvidenceWithoutThisAllele))));
-
-        return (int)(10*(-logEvidenceWithAllAlleles + logEvidenceWithoutThisAllele));
-
+        double tumorLogOdds = (-logEvidenceWithAllAlleles + logEvidenceWithoutThisAllele);
+        return tumorLogOdds;
     }
-
 }
