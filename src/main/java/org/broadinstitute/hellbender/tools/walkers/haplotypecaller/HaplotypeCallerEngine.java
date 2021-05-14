0042f893b6316e22645f659fb4b12fc4a6d6aa48
Dror Kessler <dror27.kessler@gmail.com>
2021-05-14 07:16:46 +0100

    Ultima.speedup.develop [BIOIN-138] (#47)
    * likelihood computation opimization
    
    --flow-likelihood-parallel-loop
    --flow-likelihood-optimized-comp
    
    * guard exponent from drifting too low - out of the range of double
    
    * shortcut added to likelihood comp
    
    * use dedicated thread pool. allow for setting number of threads for likelihood
    
    * assembler speedup experiemental
    
    * assembler parallelised
    
    * flow-likelihood-optimized-comp now passes integration test
    
    have to update code and modify log10 optimisation to produce exact same results
    
    * restructure back to individual tests
    
    * speedup now passed integ test
    
    * reset random generator to make running multiple tests in sequence
    
    * likelihood-optimized-comp-compatability-mode introduced
    
    used for unit testing where exact match on log10 results is required
    
    * Revert "likelihood-optimized-comp-compatability-mode introduced"
    
    This reverts commit 8827c615b7970c679f91064ead50dd37c50a8503.
    
    * log10 caching
    
    * cache most used prob values
    
    reduces log10 calls to 7%
    
    * Updated test files
    
    Co-authored-by: Ilya Soifer <ilya.soifer@ultimagen.com>

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCallerEngine.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCallerEngine.java
@@ -57,6 +57,7 @@ import org.ultimagen.haplotypeCalling.LHWRefView;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.*;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 
 import static org.broadinstitute.hellbender.utils.activityprofile.ActivityProfileState.Type.HIGH_QUALITY_SOFT_CLIPS;
@@ -168,6 +169,8 @@ public final class HaplotypeCallerEngine implements AssemblyRegionEvaluator {
     private static final Allele FAKE_REF_ALLELE = Allele.create("N", true); // used in isActive function to call into UG Engine. Should never appear anywhere in a VCF file
     private static final Allele FAKE_ALT_ALLELE = Allele.create("<FAKE_ALT>", false); // used in isActive function to call into UG Engine. Should never appear anywhere in a VCF file
 
+    private ForkJoinPool assemblerThreadPool;
+
     /**
      * Create and initialize a new HaplotypeCallerEngine given a collection of HaplotypeCaller arguments, a reads header,
      * and a reference file
@@ -207,6 +210,10 @@ public final class HaplotypeCallerEngine implements AssemblyRegionEvaluator {
 
         trimmer = new AssemblyRegionTrimmer(assemblyRegionArgs, readsHeader.getSequenceDictionary());
         initialize(createBamOutIndex, createBamOutMD5);
+
+        if ( hcArgs.assemblerArgs.flowAssemblerParallelThreads > 0 ) {
+            assemblerThreadPool = new ForkJoinPool(hcArgs.assemblerArgs.flowAssemblerParallelThreads);
+        }
     }
 
     /**
@@ -748,7 +755,7 @@ public final class HaplotypeCallerEngine implements AssemblyRegionEvaluator {
                 writer -> writer.writeAlleleLikelihoods(readLikelihoods));
 
         // Realign reads to their best haplotype.
-        final Map<GATKRead, GATKRead> readRealignments = AssemblyBasedCallerUtils.realignReadsToTheirBestHaplotype(readLikelihoods, assemblyResult.getReferenceHaplotype(), assemblyResult.getPaddedReferenceLoc(), aligner);
+        final Map<GATKRead, GATKRead> readRealignments = AssemblyBasedCallerUtils.realignReadsToTheirBestHaplotype(readLikelihoods, assemblyResult.getReferenceHaplotype(), assemblyResult.getPaddedReferenceLoc(), aligner, assemblerThreadPool);
         readLikelihoods.changeEvidence(readRealignments);
         haplotypes = readLikelihoods.alleles();
         final AlleleLikelihoods<GATKRead, Haplotype> uncollapsedReadLikelihoods;
