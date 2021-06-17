1d7165c38e658116bfd70f769d63294172ca8dbd
Dror Kessler <dror27.kessler@gmail.com>
2021-06-17 10:49:06 +0300

    Featuremapper, initial version (#53)
    * SNV Mapper, initial commit
    
    * initial version (pre-alpha)
    
    Not verified to be correct ... simply works but no testing done yet ... probably wrong.
    
    args:
    FlowFeatureMapper
    -R
    ../ref/Homo_sapiens_assembly38.fasta
    -O
    data/200916/snv_devel.vcf
    -I
    data/200916/170280-BC10.chr90.81M.bam
    --intervals
    chr9:81,817,471-81,818,122
    --smith-waterman
    FASTEST_AVAILABLE
    --likelihood-calculation-engine
    FlowBased
    -mbq
    0
    --kmer-size
    10
    --copy-attr
    tr
    --copy-attr
    tt
    
    * additional parameteres
    
    FlowFeatureMapper
    -R
    ../ref/Homo_sapiens_assembly38.fasta
    -O
    data/200916/snv_devel.vcf
    -I
    data/200916/170280-BC10.chr90.81M.bam
    --intervals
    chr9:81,817,471-81,818,122
    --smith-waterman
    FASTEST_AVAILABLE
    --likelihood-calculation-engine
    FlowBased
    -mbq
    0
    --kmer-size
    10
    --copy-attr
    tr
    --copy-attr
    tt
    --snv-identical-bases
    3
    --snv-identical-bases-after
    10
    --score-delta-max
    5
    
    * --min-score-delta added
    
    * order VCF writing
    
    * investigating NaN values.
    
    added --exclude-nan-scores boolean parameter
    
    This is still under investigation. currently 10% on test file:
    150451-BC94.aligned.sorted.duplicates_marked.chr9_80M_82M.bam
    
    * NaN resolved. Still issue (see comment)
    
    SNV located less that 10 bases from end of read are given score of -1 since the internal code for the FlowRead rejects them.
    
    This will be resolved after discussion.
    
    * remove (trimmed) read  size limitation from feature mapper
    
    * score function made local and simplified
    
    same results
    
    * read/filtered counts added
    
    * reduce -1 score by tighter limitation on flow read
    
    * additional VCF fields + include-dup-reads flag
    
    * cleanup and trimming fixes
    
    * clip negatives to 0
    
    * tracing specific reads
    
    --debug-read-name parameter (multiple allowed)
    
    * 23/5 changes
    
    Please use “--snv-identical-bases 10” so we avoid cases with alignment issues.
    Regarding varying lengths – both flow space sequences should be clipped to the length of the shorter of the two, rather than reporting inf
    Regarding a flexible flow space alignment – there shouldn’t be one. The alignment is known – we have it in base space to begin with and we are trying to judge whether this given alignment could be due to sequencing errors. We know the base previous to the SNV is aligned to the specific base in the reference that the base-space alignment suggests, and we should not allow for a shift in flow space to yield a better mapping. In the example you gave the first alignment should be taken. The fact that it’s terrible is a feature not a bug. I actually don’t understand why the case that happened here doesn’t always happen to I am waiting to discuss in person.
    
    * --keep-supplementary-alignments
    
    defaults to false, meaning supplementary alignments will not be processed
    
    * added X-FC1 and X-FC2
    
    X-FC1 - number of features on the same read
    X-FC2 - number of features on the same read that passed the filter
    
    * FC1/2, edit distance
    
    FC1 - Number of M bases different on read from references
    FC2 - Number of features before score threshold filter
    LENGTH - Read length
    EDIST - Read Levenshtein edit distance from reference
    
    * Remove soft clipped bases from edit distance
    
    * N bases should not count as different from ref
    
    * 'N' should be ignored on reference (instead of read)
    
    * featureMapper basic unit test
    
    runs the tool and verifies output against an expected result.

---
+++ src/main/java/org/ultimagen/featureMapping/FlowFeatureMapperArgumentCollection.java
@@ -0,0 +1,104 @@
+package org.ultimagen.featureMapping;
+
+import org.broadinstitute.barclay.argparser.Argument;
+import org.ultimagen.featureMapping.FlowFeatureMapper;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set of arguments for the {@link FlowFeatureMapper}
+ */
+public class FlowFeatureMapperArgumentCollection implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    enum MappingFeatureEnum {
+        SNV
+    };
+
+    /**
+     *  kind of feature we are mapping (looking for)
+     **/
+    @Argument(fullName = "mapping-feature", doc = "Kind of feaure being mapped", optional = true)
+    public MappingFeatureEnum mappingFeature = MappingFeatureEnum.SNV;
+
+    /**
+     *  maximum value for delta in score
+     **/
+    @Argument(fullName = "limit-score", doc = "Limit value for score", optional = true)
+    public double limitScore = Double.NaN;
+
+    /**
+     *  attributes to copy from bam
+     **/
+    @Argument(fullName = "copy-attr", doc = "attributes to copy from bam", optional = true)
+    public List<String> copyAttr = new LinkedList<>();
+
+    /**
+     *  prefix to add to attributes to copy from bam
+     **/
+    @Argument(fullName = "copy-attr-prefix", doc = "prefix to add to attributes to copy from bam", optional = true)
+    public String copyAttrPrefix = "";
+
+    /**
+     *  number of bases that need to be identical before the snv
+     **/
+    @Argument(fullName = "snv-identical-bases", doc = "number of bases that need to be identical before the snv", optional = true)
+    public int snvIdenticalBases = 1;
+
+    /**
+     *  number of bases that need to be identical after the snv
+     **/
+    @Argument(fullName = "snv-identical-bases-after", doc = "number of bases that need to be identical after the snv. 0 means same as number of bases before", optional = true)
+    public int snvIdenticalBasesAfter = 0;
+
+    /**
+     *  threshold of score delta to for emitting (will be emitted if lower)
+     **/
+    @Argument(fullName = "max-score", doc = "threshold of score delta to for emitting (will be emitted if lower)", optional = true)
+    public double maxScore = Double.POSITIVE_INFINITY;
+
+    /**
+     *  minimal threshold of score delta to for emitting (will be emitted if higher)
+     **/
+    @Argument(fullName = "min-score", doc = "minimal threshold of score delta to for emitting (will be emitted if higher)", optional = true)
+    public double minScore = Double.NEGATIVE_INFINITY;
+
+    /**
+     *  exclude NaN score records?
+     **/
+    @Argument(fullName = "exclude-nan-scores", doc = "exclude nan scores", optional = true)
+    public boolean excludeNaNScores = false;
+
+    /**
+     *  include duplicate read?
+     **/
+    @Argument(fullName = "include-dup-reads", doc = "include duplicate reads", optional = true)
+    public boolean includeDupReads = false;
+
+    /**
+     *  keep negatives?
+     **/
+    @Argument(fullName = "keep-negatives", doc = "keep nevative scores?", optional = true)
+    public boolean keepNegatives = false;
+
+    /**
+     *  keep supplementary alignments?
+     **/
+    @Argument(fullName = "keep-supplementary-alignments", doc = "keep supplementary alignments ?", optional = true)
+    public boolean keepSupplementaryAlignments = false;
+
+    /**
+     *  debug negatives?
+     **/
+    @Argument(fullName = "debug-negatives", doc = "debug nevative scores?", optional = true)
+    public boolean debugNegatives = false;
+
+    /**
+     *  debug read names?
+     **/
+    @Argument(fullName = "debug-read-name", doc = "debug specific reads?", optional = true)
+    public List<String> debugReadName = null;
+}
