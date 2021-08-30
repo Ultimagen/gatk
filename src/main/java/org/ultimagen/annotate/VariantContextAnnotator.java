410e784f46cb80510a0ae1dbd970a4b82a193982
Dror Kessler <dror27.kessler@gmail.com>
2021-08-30 14:32:43 +0300

    remove old code

--- src/main/java/org/ultimagen/annotate/VariantContextAnnotator.java
+++
@@ -1,8 +0,0 @@
-package org.ultimagen.annotate;
-
-import htsjdk.variant.variantcontext.VariantContext;
-
-public interface VariantContextAnnotator {
-
-    void annotate(final VariantContext vc);
-}
