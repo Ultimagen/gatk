79ade33cae0cb615850c2eb0558dc94de09ffae5
Dror Kessler <dror27.kessler@gmail.com>
2021-08-30 14:22:12 +0300

    comments removed

--- src/main/java/org/ultimagen/annotate/CycleSkipAnnotation.java
+++ src/main/java/org/ultimagen/annotate/CycleSkipAnnotation.java
@@ -18,36 +18,16 @@ public class CycleSkipAnnotation extends AnnotatorBase {
     @Override
     public void annotate(final VariantContext vc) {
 
-        /*
-        #gt_field is None
-        na_pos = df['indel'] | (df['alleles'].apply(len) > 2)
-         */
         boolean naIndicator = vc.getAttribute(A_INDEL_CLASSIFY) != null || vc.getAlleles().size() > 2;
 
-        /*
-        df['cycleskip_status'] = "NA"
-        snp_pos = ~na_pos
-        snps = df.loc[snp_pos].copy()
-        left_last = np.array(snps['left_motif']).astype(np.string_)
-        right_first = np.array(snps['right_motif']).astype(np.string_)
-         */
         String      css = C_CSS_NA;
         boolean     snpIndicator = !naIndicator;
         String      leftLast = vc.getAttributeAsString(A_LEFT_MOTIF, null);
         String      rightFirst = vc.getAttributeAsString(A_RIGHT_MOTIF, null);
 
-        /*
-        ref = np.array(snps['ref']).astype(np.string_)
-        alt = np.array(snps['alleles'].apply(
-            lambda x: x[1] if len(x) > 1 else None)).astype(np.string_)
-         */
         String      ref = vc.getReference().getBaseString();
         String      alt = (snpIndicator && (vc.getAlleles().size() > 1)) ? vc.getAlleles().get(1).getBaseString() : null;
 
-        /*
-        ref_seqs = np.char.add(np.char.add(left_last, ref), right_first)
-        alt_seqs = np.char.add(np.char.add(left_last, alt), right_first)
-         */
         String      refSeqs = null;
         String      altSeqs = null;
         if ( leftLast != null && rightFirst != null && alt != null ) {
@@ -55,24 +35,9 @@ public class CycleSkipAnnotation extends AnnotatorBase {
             altSeqs = leftLast + alt + rightFirst;
         }
 
-        /*
-        ref_encs = [utils.generateKeyFromSequence(
-                str(np.char.decode(x)), flow_order) for x in ref_seqs]
-        alt_encs = [utils.generateKeyFromSequence(
-                str(np.char.decode(x)), flow_order) for x in alt_seqs]
-         */
         int[]   refEncs = generateKeyFromSequence(refSeqs, flowOrder);
         int[]   altEncs = generateKeyFromSequence(altSeqs, flowOrder);
 
-        /*
-        cycleskip = np.array([x for x in range(len(ref_encs))
-                          if len(ref_encs[x]) != len(alt_encs[x])])
-        poss_cycleskip = [x for x in range(len(ref_encs)) if len(ref_encs[x]) == len(alt_encs[x])
-                          and (np.any(ref_encs[x][ref_encs[x] - alt_encs[x] != 0] == 0) or
-                           np.any(alt_encs[x][ref_encs[x] - alt_encs[x] != 0] == 0))]
-        s = set(np.concatenate((cycleskip, poss_cycleskip)))
-        non_cycleskip = [x for x in range(len(ref_encs)) if x not in s]
-         */
         if ( altEncs != null ) {
             boolean cycleskip = refEncs.length != altEncs.length;
             boolean passCycleskip = !cycleskip && !Arrays.equals(refEncs, altEncs);
@@ -96,22 +61,6 @@ public class CycleSkipAnnotation extends AnnotatorBase {
             return null;
         List<Integer>       key = new ArrayList<>(sequence.length() * (flowOrder.length() - 1));
 
-        /*
-        key = []
-        pos = 0
-        for base in flow:
-            hcount = 0
-            for i in range(pos, len(sequence)):
-                if sequence[i] == base:
-                    hcount+=1
-                else:
-                    break
-            else:
-                key.append(hcount)
-                break # end of sequence
-            key.append(hcount)
-            pos += hcount
-         */
         byte[]      seq = sequence.getBytes();
         byte[]      flow = flowOrder.getBytes();
         int         pos = 0;
