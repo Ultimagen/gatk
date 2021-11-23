101163c7b249b0682acaa80d77fefaa12e9d8d84
James <emeryj@broadinstitute.org>
2021-11-23 09:04:28 -0500

    true -> false
    updated a test output that was missed

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCallerIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCallerIntegrationTest.java
@@ -52,7 +52,7 @@ public class HaplotypeCallerIntegrationTest extends CommandLineProgramTest {
     // instead of actually running the tests. Can be used with "./gradlew test -Dtest.single=HaplotypeCallerIntegrationTest"
     // to update all of the exact-match tests at once. After you do this, you should look at the
     // diffs in the new expected outputs in git to confirm that they are consistent with expectations.
-    public static final boolean UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS = true;
+    public static final boolean UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS = false;
 
     public static final String TEST_FILES_DIR = toolsTestDir + "haplotypecaller/";
 
@@ -1525,11 +1525,12 @@ public class HaplotypeCallerIntegrationTest extends CommandLineProgramTest {
         final File output = createTempFile("output", ".vcf");
 
         final File expected = new File(TEST_FILES_DIR, "test_output.g.vcf");
+        final String outputPath = UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS ? expected.getAbsolutePath() : output.getAbsolutePath();
 
         final ArgumentsBuilder args = new ArgumentsBuilder()
                 .addReference(hg38Reference)
                 .addInterval("chr9:81149486-81177047")
-                .addOutput(output)
+                .addOutput(outputPath)
                 .addInput(input)
                 .add("smith-waterman", "FASTEST_AVAILABLE")
                 .add("likelihood-calculation-engine", "FlowBased")
