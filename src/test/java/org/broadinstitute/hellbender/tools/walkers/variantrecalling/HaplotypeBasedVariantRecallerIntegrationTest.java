13d1097f6c14c32714a9c0ccd4487afdab6d9248
Dror Kessler <dror27.kessler@gmail.com>
2021-12-16 11:21:19 +0000

    groundTruth integration test

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/variantrecalling/HaplotypeBasedVariantRecallerIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/variantrecalling/HaplotypeBasedVariantRecallerIntegrationTest.java
@@ -1,8 +1,6 @@
 package org.broadinstitute.hellbender.tools.walkers.variantrecalling;
 
 import org.broadinstitute.hellbender.CommandLineProgramTest;
-import org.broadinstitute.hellbender.tools.walkers.variantrecalling.FlowTestConstants;
-import org.broadinstitute.hellbender.tools.walkers.variantrecalling.TestFileVerifySame;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,7 +9,7 @@ import java.io.IOException;
 
 public class HaplotypeBasedVariantRecallerIntegrationTest extends CommandLineProgramTest {
 
-    protected static String    vcTestDir = publicTestDir + FlowTestConstants.VARIANT_CALLING_DATA_DIR;
+    private static String testDir = publicTestDir + FlowTestConstants.VARIANT_CALLING_DATA_DIR;
 
     @Test
     public void testBasic() throws IOException {
@@ -19,10 +17,10 @@ public class HaplotypeBasedVariantRecallerIntegrationTest extends CommandLinePro
         final File outputDir = createTempDir("testHaplotypeBasedVariantRecallerTest");
         final String outputPath = outputDir + "/output.csv";
         final String[] args = new String[] {
-                "--alleles-file-vcf", vcTestDir + "/150292-BC05.vcf.gz",
-                "--haplotypes-file-bam", vcTestDir + "/haps_chr5.bam",
-                "-I", vcTestDir + "/chr5.bam1.rename.bam",
-                "-I", vcTestDir + "/chr5.bam2.rename.bam",
+                "--alleles-file-vcf", testDir + "/150292-BC05.vcf.gz",
+                "--haplotypes-file-bam", testDir + "/haps_chr5.bam",
+                "-I", testDir + "/chr5.bam1.rename.bam",
+                "-I", testDir + "/chr5.bam2.rename.bam",
                 "--reference", largeFileTestDir + "/Homo_sapiens_assembly38.fasta.gz",
                 "--matrix-file-csv", outputPath,
                 "--likelihood-calculation-engine", "FlowBased",
@@ -35,6 +33,6 @@ public class HaplotypeBasedVariantRecallerIntegrationTest extends CommandLinePro
         // verify that output file has been created
         // walk the output and expected files, compare non-comment lines
         Assert.assertTrue((new File(outputPath)).exists());
-        (new TestFileVerifySame.NearlySameDoubles()).verifySame(outputPath, vcTestDir + "/bam1_bam2_output.csv");
+        (new TestFileVerifySame.NearlySameDoubles()).verifySame(outputPath, testDir + "/bam1_bam2_output.csv");
     }
 }
