fa853ad79416c0d13a112e21d4beb452a30d7249
Dror Kessler <dror27.kessler@gmail.com>
2022-01-19 10:41:42 +0000

    FlowBasedReadUtils created

---
+++ src/main/java/org/broadinstitute/hellbender/utils/read/FlowBasedReadUtils.java
@@ -0,0 +1,83 @@
+package org.broadinstitute.hellbender.utils.read;
+
+import htsjdk.samtools.SAMFileHeader;
+import htsjdk.samtools.SAMReadGroupRecord;
+
+/**
+ * utility class for flow based read
+ */
+public class FlowBasedReadUtils {
+
+    public static final int FLOW_SUM_OF_BASE_QUALITY_THRESHOLD = 15;
+
+    public static boolean readEndMarkedUncertain(final GATKRead rec) {
+        final String        tm = rec.getAttributeAsString(FlowBasedRead.CLIPPING_TAG_NAME);
+        if ( tm == null ) {
+            return false;
+        } else {
+            return tm.indexOf('Q') >= 0 || tm.indexOf('Z') >= 0;
+        }
+    }
+
+    public static boolean readEndMarkedUnclipped(final GATKRead rec, boolean FLOW_Q_IS_KNOWN_END) {
+        final String        tm = rec.getAttributeAsString(FlowBasedRead.CLIPPING_TAG_NAME);
+        if ( tm == null ) {
+            return false;
+        } else {
+            return (tm.indexOf('A') >= 0) || (FLOW_Q_IS_KNOWN_END && (tm.indexOf('Q') >= 0));
+        }
+    }
+
+    // get flow order for a specific read
+    public static byte[] getReadFlowOrder(final SAMFileHeader header, GATKRead read) {
+
+        // are we looking for a specific read group, as specified by the read?
+        final String    readGroupName = (read != null) ? read.getReadGroup() : null;
+        if ( readGroupName != null ) {
+            final SAMReadGroupRecord rg = header.getReadGroup(readGroupName);
+            if ( rg != null && rg.getFlowOrder() != null )
+                return rg.getFlowOrder().getBytes();
+        }
+
+        // if here, either no read was specified, or the read has no group, or the group is not found, or it has no flow
+        // revert to old behavior of returning the first found
+        for ( SAMReadGroupRecord rg : header.getReadGroups() ) {
+            // must match read group name?
+            String      flowOrder = rg.getFlowOrder();
+            if ( flowOrder != null ) {
+                return flowOrder.getBytes();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Computes the sum of base qualities of the given flow read.
+     */
+    public static int flowSumOfBaseQualities(final GATKRead read) {
+        if (read == null) {
+            return 0;
+        } else {
+            int sum = 0;
+
+            // access qualities and bases
+            byte[]      quals = read.getBaseQualitiesNoCopy();
+            byte[]      bases = read.getBasesNoCopy();
+
+            // loop on bases, extract qual related to homopolymer from start of homopolymer
+            int         i = 0;
+            byte        lastBase = 0;
+            byte        effectiveQual = 0;
+            for (final byte base : bases ) {
+                if ( base != lastBase )
+                    effectiveQual = quals[i];
+                if ( effectiveQual >= FLOW_SUM_OF_BASE_QUALITY_THRESHOLD )
+                    sum += effectiveQual;
+                lastBase = base;
+                i++;
+            }
+
+            return sum;
+        }
+    }
+}
