adfaf6f7f318f1e3bb204e598985694b7ccdea24
Dror Kessler <dror27.kessler@gmail.com>
2022-01-20 16:31:16 +0000

    convert hmer based filter to use helper pattern

--- src/main/java/org/broadinstitute/hellbender/engine/filters/flow/HmerQualitySymetricReadFilter.java
+++ src/main/java/org/broadinstitute/hellbender/engine/filters/flow/HmerQualitySymetricReadFilter.java
@@ -1,19 +1,13 @@
 package org.broadinstitute.hellbender.engine.filters.flow;
 
-import htsjdk.samtools.CigarElement;
-import htsjdk.samtools.CigarOperator;
-import htsjdk.samtools.SAMFileHeader;
-import org.apache.commons.lang3.tuple.Pair;
 import org.broadinstitute.hellbender.engine.filters.ReadFilter;
-import org.broadinstitute.hellbender.utils.BaseUtils;
-import org.broadinstitute.hellbender.utils.read.FlowBasedRead;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
 
 /**
  * A read filter to test if the quality values for each hmer in a flow based read form
  * a polindrome (as they should)
  */
-public class HmerQualitySymetricReadFilter extends FlowBasedHmerBasedReadFilter {
+public class HmerQualitySymetricReadFilter extends ReadFilter implements FlowBasedHmerBasedReadFilterHelper.FilterImpl  {
     private static final long serialVersionUID = 1l;
 
     public HmerQualitySymetricReadFilter() {
@@ -21,14 +15,17 @@ public class HmerQualitySymetricReadFilter extends FlowBasedHmerBasedReadFilter 
     }
 
     @Override
-    protected byte[] getValuesOfInterest(final GATKRead read) {
+    public boolean test(final GATKRead read) {
+        return FlowBasedHmerBasedReadFilterHelper.test(read, this);
+    }
+
+    @Override
+    public byte[] getValuesOfInterest(GATKRead read) {
         return read.getBaseQualitiesNoCopy();
     }
 
     @Override
-    protected boolean testHmer(final byte[] values, final int hmerStartingOffset, final int hmerLength) {
-
-        // check for symmetry
-        return isPalindrome(values, hmerStartingOffset, hmerLength);
+    public boolean testHmer(byte[] values, int hmerStartingOffset, int hmerLength) {
+        return FlowBasedHmerBasedReadFilterHelper.isPalindrome(values, hmerStartingOffset, hmerLength);
     }
 }
